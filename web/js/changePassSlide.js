0ddf8f502a4dba1281e9075557fe50d8bb166469
Littlebear <sergei.v.ivliev@gmail.com>
2020-02-07 15:41:55 +0300

    js полей ввода пароля

--- web/js/changePassSlide.js
+++ web/js/changePassSlide.js
@@ -1,8 +1,12 @@
-
 window.onload = function () {
     "use strict";
     $("#changePass").click(function () {
         $("#changePassSlider").show("slow", function () {
+            $("#changedPass").click(function (e) {
+                $("#changePassSlider").hide("slow", function () {
+                    e.preventDefault();
+                })
+            });
         });
     });
 };
