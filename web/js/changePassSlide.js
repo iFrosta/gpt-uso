6b0db882b5ed29b079fb301100f35e592dce9342
Littlebear <sergei.v.ivliev@gmail.com>
2020-02-07 21:59:39 +0300

    убрал preventDefault. С ним же отправка не работает!

--- web/js/changePassSlide.js
+++ web/js/changePassSlide.js
@@ -2,10 +2,8 @@ window.onload = function () {
     "use strict";
     $("#changePass").click(function () {
         $("#changePassSlider").show("slow", function () {
-            $("#changedPass").click(function (e) {
-                $("#changePassSlider").hide("slow", function () {
-                    e.preventDefault();
-                })
+            $("#changedPass").click(function () {
+                $("#changePassSlider").hide("slow");
             });
         });
     });
