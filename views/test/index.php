d8dd2736b0964e244942497189326abc33710a48
Littlebear <sergei.v.ivliev@gmail.com>
2020-02-20 19:47:09 +0300

    [12. Отображение теста] JS часы

--- views/test/index.php
+++ views/test/index.php
@@ -4,8 +4,19 @@
 
 $this->title = 'Тест';
 ?>
+
+<div id="time">Time: </div>
+
 <div class="site-index">
         <?= $this->render('_displaytest', [
             'model' => $model,
         ]) ?>
 </div>
+
+<script>
+    setInterval(function(){
+        $.ajax({url: "../tim.php", success: function(response){
+                $('#time').html(response)
+            }});
+    }, 1000);
+</script>
